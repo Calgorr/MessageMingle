132d3077a357b31c36af1850caaedff7f35521e9
calgor <ahomayoni684@gmail.com>
2023-09-01 23:42:53 +0330

    Add:Publish rpc endpoint method completed

--- api/proto/server/handler/serverHandler.go
+++ api/proto/server/handler/serverHandler.go
@@ -1,1 +1,33 @@
 package handler
+
+import (
+	"context"
+	pb "therealbroker/api/proto/protoGen"
+	"therealbroker/pkg/broker"
+	"time"
+)
+
+type brokerServer struct {
+	pb.UnimplementedBrokerServer
+	BrokerInstance broker.Broker
+}
+
+func (s *brokerServer) Publish(ctx context.Context, request *pb.PublishRequest) (*pb.PublishResponse, error) {
+	msg := broker.Message{
+		Body:       string(request.GetBody()),
+		Expiration: time.Duration(request.GetExpirationSeconds()),
+	}
+	id, err := s.BrokerInstance.Publish(ctx, request.GetSubject(), msg)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.PublishResponse{Id: int32(id)}, nil
+}
+
+func (s *brokerServer) Subscribe(*pb.SubscribeRequest, pb.Broker_SubscribeServer) error {
+	return nil
+}
+
+func (s *brokerServer) Fetch(context.Context, *pb.FetchRequest) (*pb.MessageResponse, error) {
+	return nil, nil
+}
